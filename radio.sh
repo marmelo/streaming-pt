72a0c54f6cf509c574e62fe237eedf722b66f8e2
Rafael Marmelo <rmarmelo@gmail.com>
2018-06-14 02:39:59 +0100

    Refactored scripts to remove  from the streams array.

--- radio.sh
+++ radio.sh
@@ -23,23 +23,23 @@ TITLES=(
 )
 
 STREAMS=(
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liveradio/antena180a/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liveradio/antena280a/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liveradio/antena380a/playlist.m3u8"
-  "$PLAYER rtmp://mcrwowza5.mcr.iol.pt/cidadefmlc/cidadefmlc.sdp" 
-  "$PLAYER rtmp://mcrwowza5.mcr.iol.pt/comerciallc/comerciallc.sdp"
-  "$PLAYER http://162.210.196.217:8070/stream.mp3"
-  "$PLAYER rtmp://mcrwowza3.mcr.iol.pt/m80lc/m80lc.sdp"
-  "$PLAYER http://19573.live.streamtheworld.com:3690/MEGA_HITSAAC"
-  "$PLAYER http://centova.radios.pt/proxy/495"
-  "$PLAYER http://centova.radios.pt:8497/stream"
-  "$PLAYER http://19573.live.streamtheworld.com:3690/RADIO_RENASCENCAAAC"
-  "$PLAYER http://19573.live.streamtheworld.com:3690/RFMAAC"
-  "$PLAYER http://ruc.midi-club.net:8000/"
-  "$PLAYER rtmp://mcrwowza6.mcr.iol.pt/smoothlc/smoothlc.sdp"
-  "$PLAYER http://tsfdirecto.tsf.pt/tsfdirecto.aac"
-  "$PLAYER -user-agent 'Mozilla/5.0 (X11; Linux x86_64)' http://mcrscast.mcr.iol.pt/vodafone"
-  "$PLAYER http://stream.radiozero.pt:8000/zero128.mp3"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liveradio/antena180a/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liveradio/antena280a/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liveradio/antena380a/playlist.m3u8"
+  "rtmp://mcrwowza5.mcr.iol.pt/cidadefmlc/cidadefmlc.sdp" 
+  "rtmp://mcrwowza5.mcr.iol.pt/comerciallc/comerciallc.sdp"
+  "http://162.210.196.217:8070/stream.mp3"
+  "rtmp://mcrwowza3.mcr.iol.pt/m80lc/m80lc.sdp"
+  "http://19573.live.streamtheworld.com:3690/MEGA_HITSAAC"
+  "http://centova.radios.pt/proxy/495"
+  "http://centova.radios.pt:8497/stream"
+  "http://19573.live.streamtheworld.com:3690/RADIO_RENASCENCAAAC"
+  "http://19573.live.streamtheworld.com:3690/RFMAAC"
+  "http://ruc.midi-club.net:8000/"
+  "rtmp://mcrwowza6.mcr.iol.pt/smoothlc/smoothlc.sdp"
+  "http://tsfdirecto.tsf.pt/tsfdirecto.aac"
+  "-user-agent 'Mozilla/5.0 (X11; Linux x86_64)' http://mcrscast.mcr.iol.pt/vodafone"
+  "http://stream.radiozero.pt:8000/zero128.mp3"
 )
 
 # check if dependencies exist
@@ -52,7 +52,7 @@ do
     for i in ${!TITLES[@]}
     do
       if [ "${TITLES[i]}" = "$radio" ]; then
-        eval ${STREAMS[i]}
+        $PLAYER ${STREAMS[i]}
         break
       fi
     done
--- tv.sh
+++ tv.sh
@@ -25,25 +25,25 @@ TITLES=(
 )
 
 STREAMS=(
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtp1.smil/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtp2.smil/playlist.m3u8"
-  "$PLAYER http://live.impresa.pt/live/sic/sic540p.m3u8"
-  "$PLAYER $(wget http://tviplayer.iol.pt/direto/TVI -O - -o /dev/null | grep videoUrl\ = |cut -d\" -f2|head -n 1)"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/livetvhlsDVR/rtpndvr.smil/playlist.m3u8"
-  "$PLAYER http://live.impresa.pt/live/sicnot/sicnot540p.m3u8"
-  "$PLAYER $(wget http://tviplayer.iol.pt/direto/TVI24 -O - -o /dev/null | grep videoUrl\ = |cut -d\" -f2|head -n 1)"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtp1HD.smil/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpmem.smil/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpi.smil/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpmadeira.smil/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpacores.smil/playlist.m3u8"
-  "$PLAYER --referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpafrica.smil/playlist.m3u8"
-  "$PLAYER http://live.impresa.pt/live/sicrad/sicrad540p.m3u8"
-  "$PLAYER http://live.impresa.pt/live/sick/sick540p.m3u8"
-  "$PLAYER http://193.126.16.68:1935/livenlin4/mp4:2liveplncleanpub/playlist.m3u8"
-  "$PLAYER rtmp://213.13.26.13/live/portocanal"
-  "$PLAYER http://euronews-pt-p7-cdn.hexaglobe.net/94ce3a040643007dd3c2950436603334/5a2f43b0/euronews/euronews-euronews-website-web-responsive-2/ewnsabrptpri_por.smil/playlist.m3u8"
-  "$PLAYER http://195.22.11.11:1935/ktv/ktv2/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtp1.smil/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtp2.smil/playlist.m3u8"
+  "http://live.impresa.pt/live/sic/sic540p.m3u8"
+  "$(wget http://tviplayer.iol.pt/direto/TVI -O - -o /dev/null | grep videoUrl\ = |cut -d\" -f2|head -n 1)"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/livetvhlsDVR/rtpndvr.smil/playlist.m3u8"
+  "http://live.impresa.pt/live/sicnot/sicnot540p.m3u8"
+  "$(wget http://tviplayer.iol.pt/direto/TVI24 -O - -o /dev/null | grep videoUrl\ = |cut -d\" -f2|head -n 1)"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtp1HD.smil/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpmem.smil/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpi.smil/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpmadeira.smil/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpacores.smil/playlist.m3u8"
+  "--referrer http://www.rtp.pt https://streaming-live.rtp.pt/liverepeater/smil:rtpafrica.smil/playlist.m3u8"
+  "http://live.impresa.pt/live/sicrad/sicrad540p.m3u8"
+  "http://live.impresa.pt/live/sick/sick540p.m3u8"
+  "http://193.126.16.68:1935/livenlin4/mp4:2liveplncleanpub/playlist.m3u8"
+  "rtmp://213.13.26.13/live/portocanal"
+  "http://euronews-pt-p7-cdn.hexaglobe.net/94ce3a040643007dd3c2950436603334/5a2f43b0/euronews/euronews-euronews-website-web-responsive-2/ewnsabrptpri_por.smil/playlist.m3u8"
+  "http://195.22.11.11:1935/ktv/ktv2/playlist.m3u8"
 )
 
 # check if dependencies exist
@@ -56,7 +56,7 @@ do
     for i in ${!TITLES[@]}
     do
       if [ "${TITLES[i]}" = "$choice" ]; then
-        eval ${STREAMS[i]}
+        $PLAYER ${STREAMS[i]}
         break
       fi
     done
