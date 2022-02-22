bb62871a28eee762a708d538c36a0c14d315a527
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-02-22 15:46:18 +0000

    New URL for Euronews (#63)
    No longer requires YouTube.

--- tv.sh
+++ tv.sh
@@ -38,7 +38,7 @@ STREAMS=(
   "http://live.impresa.pt/live/sicint/sicint.m3u8"
   "https://playout175.livextend.cloud/livenlin4/2liveartvpub/playlist.m3u8"
   "--referrer='http://sapo.pt/' https://streamer-b02.videos.sapo.pt/live/portocanal/playlist.m3u8"
-  "__euronews"
+  "https://rakuten-euronews-8-pt.samsung.wurl.com/manifest/playlist.m3u8"
   "http://195.22.11.11:1935/ktv/ktv2/playlist.m3u8"
 )
 
@@ -49,7 +49,6 @@ function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 __rtp() { urldecode $(wget https://www.rtp.pt/play/direto/$1 -O - -o /dev/null | grep hls | sed 's/.*hls: //g' | sed 's/, dash.*//g' | sed 's/.*\[//g' | sed 's/\].*//g' | sed 's/[",]//g'); }
 __tvi() { echo "https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)"; }
 __tvi24() { echo "https://video-auth6.iol.pt/live_tvi24/live_tvi24/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)"; }
-__euronews() { echo "https://www.youtube.com/watch?v=$(wget http://pt.euronews.com/api/watchlive.json -O - -o /dev/null | cut -d\" -f8)"; }
 
 #random string for user-agent to avoid blacklists
 USER_AGENT=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 50)
