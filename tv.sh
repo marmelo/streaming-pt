6e4f5653778b7234a3bd6b41979501904a3b12ea
dfilipeloja <dfilipeloja@gmail.com>
2022-12-30 19:18:34 +0000

    Canal 11: Fixed URL

--- tv.sh
+++ tv.sh
@@ -44,7 +44,7 @@ STREAMS=(
   "https://playout175.livextend.cloud/livenlin4/2liveartvpub/playlist.m3u8"
   "--referrer='http://sapo.pt/' https://streamer-b02.videos.sapo.pt/live/portocanal/playlist.m3u8"
   "https://rakuten-euronews-8-pt.samsung.wurl.com/manifest/playlist.m3u8"
-  "https://d2ve4fchffi4n1.cloudfront.net/out/v1/df356edd16f3434ab417f2c48cb1d516/index.m3u8"
+  "https://list.iptvcat.com/my_list/s/c6cca308c6fd961c3e7d93801f5908bc.m3u8"
   "http://195.22.11.11:1935/ktv/ktv2/playlist.m3u8"
 )
 
