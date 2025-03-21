aba554b09e5479fd4bc07eb222bf5e4df5af22db
Marcos Marado <mindboosternoori@gmail.com>
2025-03-21 15:24:38 +0000

    Adicionada a Rádio Douro Nacional (#49)
    * Adicionada a Rádio Douro Nacional
    
    * Fix Euronews
    
    * radio: endereço do stream da RDN actualizado
    
    ---------
    
    Co-authored-by: Pedro Lima <49299328+LITUATUI@users.noreply.github.com>

--- radio.sh
+++ radio.sh
@@ -22,6 +22,7 @@ TITLES=(
   "Smooth"
   "TSF"
   "Rádio Clube de Lamego"
+  "Rádio Douro Nacional"
 )
 
 STREAMS=(
@@ -44,6 +45,7 @@ STREAMS=(
   "http://mcrwowza3.mcr.iol.pt/smooth/smil:smooth.smil/playlist.m3u8"
   "http://tsfdirecto.tsf.pt/tsfdirecto.aac"
   "http://109.71.41.6:8110/stream"
+  "https://sp0.redeaudio.com/9796/stream"
 )
 
 # check if dependencies exist
